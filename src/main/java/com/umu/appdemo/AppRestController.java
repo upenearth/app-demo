5af45bd6625377bb7b88e216e9c76c0a6af98c20
Upendra <upendra@unific.com>
2025-05-06 16:00:12 +0530

    App Demo initial commit

---
+++ src/main/java/com/umu/appdemo/AppRestController.java
@@ -0,0 +1,15 @@
+package com.umu.appdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class AppRestController {
+
+    @GetMapping
+    public String getMessage() {
+        return "Hello-World";
+    }
+}
